a5c76ef92bed3ee92e608dced07727967a957d6c
Rm_Rashan <info.rmrashan>
2024-09-29 19:54:21 +0530

    token updates2

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
             return response()->json([
                     'access_token' => $token,
                     'token_type' => 'Bearer',
-                    'user' => $user,
+                    // 'user' => $user,
                     'status' => 'success'
                 ]);
 
